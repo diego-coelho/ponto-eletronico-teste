da64fbadb76183384d8c99c3e6e616846ac907b3
Diego Coelho da Silva <DATA53976@datainfo.inf.br>
2023-10-27 15:03:45 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ponto Eletrônico',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
